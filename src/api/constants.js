a32db07e364eb9860d8c11d684c0e238220d2b73
evan <wjh50940@163.com>
2021-11-21 14:06:44 +0800

    delete tags API

--- src/api/constants.js
+++ src/api/constants.js
@@ -3,7 +3,6 @@ const API = {
   API_REGISTER: '/register',
   API_ARTICLE: '/articles',
   API_CATEGORY: '/categories',
-  API_TAG: '/tags',
   API_USER: '/users',
   API_OPTION: '/options',
   API_UPLOAD: '/upload'
